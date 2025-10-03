5decd31b5d1c34def7bcf1d3ba0eb5ed60dadca0
hassa <hassa@LAPTOP-U03CN6LR>
2025-10-03 11:39:28 +0330

    add scalar

--- src/Basket/Program.cs
+++ src/Basket/Program.cs
@@ -1,20 +1,21 @@
 using Basket.Bootstraper;
 using Basket.Endpoints;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.ConfigureDatabase();
 
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
-
 
 app.MapCreateBasketEndpoint();
 app.MapMoveToNextEndpoint();
